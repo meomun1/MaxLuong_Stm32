838d87770311cd57ee31e2e00ee8263ae6eda4b2
meomun1 <maluong1504@gmail.com>
2024-05-07 13:43:14 +0700

    for testing timer purpose

---
+++ stmLab2/Ex1/Core/Inc/software_timer.h
@@ -0,0 +1,16 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Mar 26, 2024
+ *      Author: ChuongNM2
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun1();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
